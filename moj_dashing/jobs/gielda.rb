10a8124498d5cbc58774593559fabc0ffb70eec6
Bartłomiej Derda <bderda@opera.com>
2016-05-23 16:24:01 +0200

    small improvements

--- moj_dashing/jobs/gielda.rb
+++ moj_dashing/jobs/gielda.rb
@@ -3,12 +3,21 @@ require 'net/http'
 require 'json'
 
 # uri = URI('http://www.money.pl/gielda/gpw/akcje/')
+
+# bots = [
+#         {bot_name: 'iPad 2', bot_id: '9f8c344804e3aa04a080d51eb1d94fe5', target: 'robo1'},
+#         {bot_name: 'iPhone 6s', bot_id: '9f8c344804e3aa04a080d51eb1d953f6', target: 'robo2'},
+#         {bot_name: 'iPhone 6+', bot_id: '9f8c344804e3aa04a080d51eb1d95b60', target: 'robo3'},
+#         {bot_name: 'Unit tests', bot_id: '9f8c344804e3aa04a080d51eb1d96e40', target: 'robo5'},
+#         {bot_name: 'iPad Air 2', bot_id: '9f8c344804e3aa04a080d51eb1f97656', target: 'robo4'}
+#        ]
+
 money = URI.parse("http://www.money.pl/")
 puts money
 last = 0
 akcja = [
-  {:nazwa => "URSUS", :akcja_id => "URS" },  
-  {:nazwa => 'PKN ORLEN', :akcja_id => 'PKN'}
+  {nazwa: 'URSUS', akcja_id: 'URS' },
+  {nazwa: 'PKN ORLEN', akcja_id:'PKN'}
 ]
 
 def get_content_of_page(money)
@@ -24,6 +33,7 @@ SCHEDULER.every '1m', :first_in => 0 do
   page.search('//@ac').remove
   temp = page.css('.tr1','.tr2')
   temp.each do |post|
+    status = 4
     if !post.at_css('.link').nil?
       if  !post.at_css('.r_bz').nil?
         kurs = post.at_css('.r_bz').content.to_s
@@ -34,12 +44,20 @@ SCHEDULER.every '1m', :first_in => 0 do
       else
         next # skip empty items
       end
+      puts post[:nazwa]
       nazwa = post.at_css('.link').content.to_s
       akcja.each do |papier|
-    	if papier[:nazwa] == nazwa
-	  puts nazwa + " " + kurs
-	end
-      end # end of loop for checking each akcja  
-    end   
+        if papier[:nazwa] == nazwa
+          puts nazwa + " " + kurs # only for debuging
+          str = kurs
+          status = 1 # bo be detailed
+          data = {:result => status, :info => str, :title => post[:nazwa]}
+          puts data
+          puts post[:akcja_id]
+          send_event(post[:akcja_id], data)
+        end
+      end # end of loop for checking each akcja
+    end
+
   end
 end
